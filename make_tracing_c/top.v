79b22d61305c1accc8ec5d6404bca82f3728e331
Ed Vidal Jr <develone@sbcglobal.net>
2024-01-14 11:14:57 -0700

    devel@pi4-30:~/virtual-python-xstools/verilator-tests/make_tracing_c $ make
    -- Verilator tracing example
    
    -- VERILATE ----------------
    verilator -cc --exe -x-assign fast -Wall --trace --assert --coverage -f input.vc top.v sim_main.cpp
    
    -- BUILD -------------------
    make -j -C obj_dir -f ../Makefile_obj
    make[1]: Entering directory '/home/devel/virtual-python-xstools/verilator-tests/make_tracing_c/obj_dir'
    make[1]: Nothing to be done for 'default'.
    make[1]: Leaving directory '/home/devel/virtual-python-xstools/verilator-tests/make_tracing_c/obj_dir'
    
    -- RUN ---------------------
    obj_dir/Vtop +trace
    [0] Tracing to logs/vlt_dump.vcd...
    
    [0] Model running...
    
    [1] clk=1 rstl=1 iquad=1234 -> oquad=1235 owide=3_22222222_11111112
    [2] clk=0 rstl=0 iquad=1246 -> oquad=0 owide=0_00000000_00000000
    from counter module, t=    0, q= 0
    [3] clk=1 rstl=0 iquad=1246 -> oquad=0 owide=0_00000000_00000000
    [4] clk=0 rstl=0 iquad=1258 -> oquad=0 owide=0_00000000_00000000
    from counter module, t=    0, q= 1
    [5] clk=1 rstl=0 iquad=1258 -> oquad=0 owide=0_00000000_00000000
    [6] clk=0 rstl=0 iquad=126a -> oquad=0 owide=0_00000000_00000000
    from counter module, t=    0, q= 2
    [7] clk=1 rstl=0 iquad=126a -> oquad=0 owide=0_00000000_00000000
    [8] clk=0 rstl=0 iquad=127c -> oquad=0 owide=0_00000000_00000000
    from counter module, t=    0, q= 3
    [9] clk=1 rstl=0 iquad=127c -> oquad=0 owide=0_00000000_00000000
    [10] clk=0 rstl=1 iquad=128e -> oquad=128f owide=3_22222222_11111112
    from counter module, t=    0, q= 4
    [11] clk=1 rstl=1 iquad=128e -> oquad=128f owide=3_22222222_11111112
    [12] clk=0 rstl=1 iquad=12a0 -> oquad=12a1 owide=3_22222222_11111112
    from counter module, t=    0, q= 5
    [13] clk=1 rstl=1 iquad=12a0 -> oquad=12a1 owide=3_22222222_11111112
    [14] clk=0 rstl=1 iquad=12b2 -> oquad=12b3 owide=3_22222222_11111112
    from counter module, t=    0, q= 6
    [15] clk=1 rstl=1 iquad=12b2 -> oquad=12b3 owide=3_22222222_11111112
    [16] clk=0 rstl=1 iquad=12c4 -> oquad=12c5 owide=3_22222222_11111112
    from counter module, t=    0, q= 7
    *-* All Finished *-*
    - sub.v:29: Verilog $finish
    [17] clk=1 rstl=1 iquad=12c4 -> oquad=12c5 owide=3_22222222_11111112
    
    -- COVERAGE ----------------
    verilator_coverage --annotate logs/annotated logs/coverage.dat
    Total coverage (4/301) 1.00%
    See lines with '%00' in logs/annotated
    
    -- DONE --------------------
    To see waveforms, open vlt_dump.vcd in a waveform viewer

--- make_tracing_c/top.v
+++ make_tracing_c/top.v
@@ -7,7 +7,7 @@
 
 // This is intended to be a complex example of several features, please also
 // see the simpler examples/make_hello_c.
-
+`timescale 1ns/10ps
 module top
   (
    // Declare some signals so we can see how I/O works
@@ -25,6 +25,48 @@ module top
 wire [3:0] q;
 /* verilator lint_off UNUSEDSIGNAL */
 counter counter (.clk(clk), .q(q));
+/* verilator lint_off UNDRIVEN */
+reg host_intf_rst_i;
+
+wire host_intf_rd_i;
+wire host_intf_wr_i;
+wire [23:0] host_intf_addr_i;
+wire [15:0] host_intf_data_i;
+wire [15:0] host_intf_data_o;
+wire host_intf_done_o;
+wire host_intf_rdPending_o;
+wire sd_intf_cke;
+wire sd_intf_cs;
+wire sd_intf_cas;
+wire sd_intf_ras;
+wire sd_intf_we;
+wire [1:0] sd_intf_bs;
+wire [12:0] sd_intf_addr;
+wire sd_intf_dqml;
+wire sd_intf_dqmh;
+wire [15:0] sd_intf_dq;
+/* verilator lint_off UNDRIVEN */
+sdram_cntl sdrami(
+    clk,
+    host_intf_rst_i,
+    host_intf_rd_i,
+    host_intf_wr_i,
+    host_intf_addr_i,
+    host_intf_data_i,
+    host_intf_data_o,
+    host_intf_done_o,
+    host_intf_rdPending_o,
+    sd_intf_cke,
+    sd_intf_cs,
+    sd_intf_cas,
+    sd_intf_ras,
+    sd_intf_we,
+    sd_intf_bs,
+    sd_intf_addr,
+    sd_intf_dqml,
+    sd_intf_dqmh,
+    sd_intf_dq
+);
    // Connect up the outputs, using some trivial logic
    assign out_small = ~reset_l ? '0 : (in_small + 2'b1);
    assign out_quad  = ~reset_l ? '0 : (in_quad + 40'b1);
